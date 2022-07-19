8f3d595c48257dedf0933e403242ed20d024ce0e
Hristo Ganchev <hristo.iliev.ganchev@gmail.com>
2022-07-19 23:29:44 +0300

    add: C# project and OOP example object Flowers

---
+++ app/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OOP.Flowers myFlower = new OOP.Flowers(); // example of instantiation of the object(class)
+            Console.WriteLine(myFlower.Color);
+            myFlower.LeafsCount();
+            myFlower.AddWater();
+
+            OOP.Rose myRose = new OOP.Rose(); // example of instantiation chiled class
+            Console.WriteLine(myRose.Color);
+            myRose.LeafsCount();
+
+            OOP.Orchid myOrchid = new OOP.Orchid(); // example of instantiation chiled class
+            Console.WriteLine(myOrchid.Color);
+            myOrchid.LeafsCount();
+
+            OOP.Daisy myDaisy = new OOP.Daisy(); // example of instantiation chiled class
+            Console.WriteLine(myDaisy.Color);
+            myDaisy.AddWater();
+        }
+    }
+}
